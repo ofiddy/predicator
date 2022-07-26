dcd123d17ef88bfff7d3d929bd133aa7a9208d4e
ofiddy <108938658+ofiddy@users.noreply.github.com>
2022-07-26 13:05:40 +0100

    Final modals added.

---
+++ scripts/formulas.js
@@ -0,0 +1,8 @@
+class BasicFormula {
+    // A blank formula, represents an empty for formula input
+    // Main purpose is to be inherited
+    
+    constructor () {
+
+    }
+}
--- scripts/modals_test.js
+++ scripts/modals_test.js
@@ -9,5 +9,35 @@ function orIntModalCreate() {
     leftPanel.appendChild(document.getElementById("modal-or-int-left-panel"));
 }
 
+function andElimModalCreate() {
+    let modalToplevel = document.getElementById("modal-toplevel");
+    modalToplevel.style.display = "block";
+    
+    modalToplevel.innerHTML = "";
+    modalToplevel.appendChild(document.getElementById("modal-and-elim"));
+    modalToplevel.querySelector("#modal-and-elim").style.display = "block";
+}
+
+function impIntModalCreate() {
+    let modalToplevel = document.getElementById("modal-toplevel");
+    modalToplevel.style.display = "block";
+    
+    let leftPanel = modalToplevel.querySelector("#modal-formula-input-left-panel");
+    leftPanel.innerHTML = "";
+    leftPanel.appendChild(document.getElementById("modal-imp-int-left-panel"));
+}
+
+function varEnterModalCreate() {
+    let modalToplevel = document.getElementById("modal-toplevel");
+    modalToplevel.style.display = "block";
+    
+    modalToplevel.innerHTML = "";
+    modalToplevel.appendChild(document.getElementById("modal-var-enter"));
+    modalToplevel.querySelector("#modal-var-enter").style.display = "block";
+}
+
 orIntModalSetup();
-orIntModalCreate();
+//impIntModalCreate();
+//orIntModalCreate();
+//andElimModalCreate();
+//varEnterModalCreate();
