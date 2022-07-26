da55c78e12c62e0c95e20ca401ce281ecb61ce4e
ofiddy <108938658+ofiddy@users.noreply.github.com>
2022-07-26 12:07:39 +0100

    Restructured modal handling

--- scripts/modals_test.js
+++ scripts/modals_test.js
@@ -1,3 +1,13 @@
 import {orIntModalSetup} from "./modals.js"
 
+function orIntModalCreate() {
+    let modalToplevel = document.getElementById("modal-toplevel");
+    modalToplevel.style.display = "block";
+    
+    let leftPanel = modalToplevel.querySelector("#modal-formula-input-left-panel");
+    leftPanel.innerHTML = "";
+    leftPanel.appendChild(document.getElementById("modal-or-int-left-panel"));
+}
+
 orIntModalSetup();
+orIntModalCreate();
